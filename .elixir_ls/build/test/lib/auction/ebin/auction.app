2ee8d3bcd514527475738f510fc29e2c12448485
kamalogudah <mcpaul2058@gmail.com>
2019-06-01 11:56:09 +0300

    Added base auction functionality

---
+++ .elixir_ls/build/test/lib/auction/ebin/auction.app
@@ -0,0 +1,8 @@
+{application,auction,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"auction"},
+              {modules,['Elixir.Auction','Elixir.Auction.Application',
+                        'Elixir.Auction.FakeRepo','Elixir.Auction.Item']},
+              {registered,[]},
+              {vsn,"0.1.0"},
+              {mod,{'Elixir.Auction.Application',[]}}]}.
--- apps/auction/lib/auction.ex
+++ apps/auction/lib/auction.ex
@@ -2,17 +2,29 @@ defmodule Auction do
   @moduledoc """
   Documentation for Auction.
   """
+  alias Auction.{FakeRepo, Item}
+
+  @repo FakeRepo
 
   @doc """
-  Hello world.
+  List Items.
 
   ## Examples
 
-      iex> Auction.hello()
+      iex> Auction.list_items()
       :world
 
   """
-  def hello do
-    :world
+
+  def list_items do
+    @repo.all(Item)
+  end
+
+  def get_item(id) do
+    @repo.get!(Item, id)
+  end
+
+  def get_item_by(attrs) do
+    @repo.get_by(Item, attrs)
   end
 end
