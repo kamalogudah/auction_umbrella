6e40e08f02157730c0728ade4acaa3a81b9868c0
kamalogudah <mcpaul2058@gmail.com>
2019-06-02 12:24:27 +0300

    Listing Items from the fake repo

--- config/config.exs
+++ config/config.exs
@@ -2,12 +2,23 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :auction_web,
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :auction_web, AuctionWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "6op4nTIG6+/QrlYIXtP6POZ0MNQJKspKpVfn+oa2fARi+uahB5paLWjbXKy4QL0s",
+  render_errors: [view: AuctionWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: AuctionWeb.PubSub, adapter: Phoenix.PubSub.PG2]
+
 # By default, the umbrella project as well as each child
 # application will require this configuration file, ensuring
 # they all use the same configuration. While one could
 # configure all applications here, we prefer to delegate
 # back to each application for organization purposes.
 import_config "../apps/*/config/config.exs"
+config :phoenix, :json_library, Jason
 
 # Sample configuration (overrides the imported configuration above):
 #
