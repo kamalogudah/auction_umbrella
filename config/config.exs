df3248a1004faf33ba898552c3c750bfa968b545
kamalogudah <mcpaul2058@gmail.com>
2019-06-02 13:12:01 +0300

    Getting app to work

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,8 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
+import_config "../apps/*/config/config.exs"
+config :phoenix, :json_library, Jason
 
 config :auction_web,
   generators: [context_app: false]
@@ -8,6 +10,7 @@ config :auction_web,
 # Configures the endpoint
 config :auction_web, AuctionWeb.Endpoint,
   url: [host: "localhost"],
+  http: [port: 4000],
   secret_key_base: "6op4nTIG6+/QrlYIXtP6POZ0MNQJKspKpVfn+oa2fARi+uahB5paLWjbXKy4QL0s",
   render_errors: [view: AuctionWeb.ErrorView, accepts: ~w(html json)],
   pubsub: [name: AuctionWeb.PubSub, adapter: Phoenix.PubSub.PG2]
@@ -17,8 +20,6 @@ config :auction_web, AuctionWeb.Endpoint,
 # they all use the same configuration. While one could
 # configure all applications here, we prefer to delegate
 # back to each application for organization purposes.
-import_config "../apps/*/config/config.exs"
-config :phoenix, :json_library, Jason
 
 # Sample configuration (overrides the imported configuration above):
 #
