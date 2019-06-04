c0ca8173caa2c69a637dab19bfe7b3e44e5d67d5
kamalogudah <mcpaul2058@gmail.com>
2019-06-04 13:51:25 +0300

    Handling errors in forms

--- apps/auction_web/lib/auction_web/controllers/item_controller.ex
+++ apps/auction_web/lib/auction_web/controllers/item_controller.ex
@@ -17,7 +17,9 @@ defmodule AuctionWeb.ItemController do
   end
 
   def create(conn, %{"item" => item_params}) do
-    {:ok, item} = Auction.insert_item(item_params)
-    redirect(conn, to: Routes.item_path(conn, :show, item))
+    case Auction.insert_item(item_params) do
+      {:ok, item} -> redirect(conn, to: Routes.item_path(conn, :show, item))
+      {:error, item} -> render(conn, "new.html", item: item)
+    end
   end
 end
