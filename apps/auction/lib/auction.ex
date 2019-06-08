a68d3bfc8370fdc8b6e09dab337a31a4a503eca4
kamalogudah <mcpaul2058@gmail.com>
2019-06-08 12:53:36 +0300

    Session and Authentication

--- apps/auction/lib/auction.ex
+++ apps/auction/lib/auction.ex
@@ -2,7 +2,7 @@ defmodule Auction do
   @moduledoc """
   Documentation for Auction.
   """
-  alias Auction.{Repo, Item, User}
+  alias Auction.{Repo, Item, User, Password}
 
   @repo Repo
 
@@ -56,5 +56,14 @@ defmodule Auction do
     %User{}
     |> User.changeset_with_password(params)
     |> @repo.insert
+  end
+
+  def get_user_by_username_and_password(username, password) do
+    with user when not is_nil(user) <- @repo.get_by(User, %{username: username}),
+         true <- Password.verify_with_hash(password, user.hashed_password) do
+      user
+    else
+      _ -> Password.dummy_verify()
+    end
   end
 end
