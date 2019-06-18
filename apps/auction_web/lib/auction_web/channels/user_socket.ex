cbf099850846dd8e7a80c4062fba5047b724ee30
kamalogudah <mcpaul2058@gmail.com>
2019-06-18 23:39:51 +0300

    Connecting a user to a channel and a topic

--- apps/auction_web/lib/auction_web/channels/user_socket.ex
+++ apps/auction_web/lib/auction_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule AuctionWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", AuctionWeb.RoomChannel
+  channel "item:*", AuctionWeb.ItemChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
