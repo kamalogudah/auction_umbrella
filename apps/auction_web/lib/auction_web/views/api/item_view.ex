bf4afe749cf5d8c3dac87a84bd756d42087ab6b8
kamalogudah <mcpaul2058@gmail.com>
2019-06-24 15:51:52 +0300

    Including Related bid and user data

--- apps/auction_web/lib/auction_web/views/api/item_view.ex
+++ apps/auction_web/lib/auction_web/views/api/item_view.ex
@@ -4,7 +4,18 @@ defmodule AuctionWeb.Api.ItemView do
     %{data: render_many(items, __MODULE__, "item.json")}
   end
   def render("show.json", %{item: item}) do
-    %{data: render_one(item, __MODULE__, "item.json")}
+    %{data: render_one(item, __MODULE__, "item_with_bids.json")}
+  end
+
+  def render("item_with_bids.json", %{item: item}) do
+    %{
+      type: "item",
+      id: item.id,
+      title: item.title,
+      description: item.description,
+      ends_at: item.ends_at,
+      bids: render_many(item.bids, AuctionWeb.Api.BidView, "bid.json")
+    }
   end
   def render("item.json", %{item: item}) do
 
@@ -17,6 +28,5 @@ defmodule AuctionWeb.Api.ItemView do
       }
 
   end
-
 
 end
